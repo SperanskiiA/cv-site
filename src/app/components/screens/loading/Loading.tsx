8e86b0103b2f61af2c0a2ff55e6e53fff0d4fbd9
Speranskii-a <speranskii_a@mail.ru>
2023-04-14 18:29:44 +0300

    added lottie loader

--- src/app/components/screens/loading/Loading.tsx
+++ src/app/components/screens/loading/Loading.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './Loading.module.scss';
+import { Player } from '@lottiefiles/react-lottie-player';
+import * as lottie from '../../../assets/lotties/141443-two-dots-loading-loader.json';
 export const Loading = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.flex}>
-        <div>Loading</div>
+        <Player
+          src={lottie}
+          className={styles.lottie}
+          autoplay={true}
+          loop={true}
+        />
       </div>
     </div>
   );
